e906873967f653bdf7acafe9b6379259a63311b6
salem malick mohamed <saloumfall45@gmail.com>
2023-10-03 15:42:15 +0000

    side menu okay

--- interact.js
+++ interact.js
@@ -6,3 +6,17 @@ window.addEventListener('scroll', () => {
         backtop.style.opacity="0";
     }
 })
+
+
+// side menu 
+let menu_door=document.getElementById('menu-door');
+menu_door.addEventListener('click',()=>{
+    let side_menu=document.getElementById('side-menu');
+    if (side_menu.style.marginLeft=='-400px') {
+        
+        side_menu.style.marginLeft="0";
+    }else{
+
+        side_menu.style.marginLeft="-400px";
+    }
+})

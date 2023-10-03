a4c5598ff8c43048e9ba731126caeaf480116a1c
salem malick mohamed <saloumfall45@gmail.com>
2023-10-03 22:41:25 +0000

    product display list or grid okay

--- interact.js
+++ interact.js
@@ -18,3 +18,26 @@ menu_door.addEventListener('click', () => {
         side_menu.style.marginLeft = "-400px";
     }
 })
+
+// product sort
+
+let items_d_grid = document.getElementById('items-d-grid');
+let items_d_list = document.getElementById('items-d-list');
+
+let list_button = document.querySelectorAll('#list-door');
+let grid_button = document.querySelectorAll('#grid-door');
+
+list_button.forEach(element => {
+
+    element.addEventListener('click', () => {
+        items_d_grid.classList.add('d-none');
+        items_d_list.classList.remove('d-none');
+    })
+})
+grid_button.forEach(element => {
+
+    element.addEventListener('click', () => {
+        items_d_list.classList.add('d-none');
+        items_d_grid.classList.remove('d-none');
+    })
+})

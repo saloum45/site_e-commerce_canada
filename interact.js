8d9a0cf324fc39fce63bc6c266fc4d70e70a5d5b
salem malick mohamed <saloumfall45@gmail.com>
2023-10-03 22:08:57 +0000

    products sort in process

--- interact.js
+++ interact.js
@@ -1,22 +1,20 @@
 let backtop = document.getElementById('back-top');
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 300) {
-        backtop.style.opacity="1";
+        backtop.style.opacity = "1";
     } else {
-        backtop.style.opacity="0";
+        backtop.style.opacity = "0";
     }
 })
 
 
 // side menu 
-let menu_door=document.getElementById('menu-door');
-menu_door.addEventListener('click',()=>{
-    let side_menu=document.getElementById('side-menu');
-    if (side_menu.style.marginLeft=='-400px') {
-        
-        side_menu.style.marginLeft="0";
-    }else{
-
-        side_menu.style.marginLeft="-400px";
+let menu_door = document.getElementById('menu-door');
+menu_door.addEventListener('click', () => {
+    let side_menu = document.getElementById('side-menu');
+    if (side_menu.style.marginLeft == '-400px') {
+        side_menu.style.marginLeft = "0";
+    } else {
+        side_menu.style.marginLeft = "-400px";
     }
 })
